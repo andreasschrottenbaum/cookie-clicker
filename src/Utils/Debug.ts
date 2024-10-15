380910b91dec3c0879d6c4f4e8ddfc26c4ed2efe
Andreas Schrottenbaum <andreas.schrottenbaum@3q.video>
2024-10-15 05:21:10 +0200

    extended debug constructor signature

--- src/Utils/Debug.ts
+++ src/Utils/Debug.ts
@@ -12,16 +12,15 @@ class Debug {
   public clear: any;
   public count: any;
 
-  constructor(label: string = '') {
-    const PREFIX = 'CCHS'
+  constructor(label: string = '', style='color: purple; font-weight: bold', prefix='CCHS') {
 
-    this.log = Function.prototype.bind.call(console.log, console, `[${PREFIX}: %c${label}%c]`, 'color: rebeccapurple; font-weight: bold', '');
-    this.warn = Function.prototype.bind.call(console.warn, console, `[${PREFIX}: %c${label}%c]`, 'color: orange; font-weight: bold', '');
-    this.error = Function.prototype.bind.call(console.error, console, `[${PREFIX}: %c${label}%c]`, 'color: red; font-weight: bold', '');
-    this.info = Function.prototype.bind.call(console.info, console, `[${PREFIX}: %c${label}%c]`, 'color: blue; font-weight: bold', '');
-    this.assert = Function.prototype.bind.call(console.assert, console, `[${PREFIX}: %c${label}%c]`, 'color: orange; font-weight: bold', '');
-    this.trace = Function.prototype.bind.call(console.trace, console, `[${PREFIX}: %c${label}%c]`, 'color: green; font-weight: bold', '');
-    this.group = Function.prototype.bind.call(console.group, console, `[${PREFIX}: %c${label}%c]`, 'color: purple; font-weight: bold', '');
+    this.log = Function.prototype.bind.call(console.log, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.warn = Function.prototype.bind.call(console.warn, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.error = Function.prototype.bind.call(console.error, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.info = Function.prototype.bind.call(console.info, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.assert = Function.prototype.bind.call(console.assert, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.trace = Function.prototype.bind.call(console.trace, console, `[%c${prefix}: ${label}%c]`, style, '');
+    this.group = Function.prototype.bind.call(console.group, console, `[%c${prefix}: ${label}%c]`, style, '');
     this.groupEnd = Function.prototype.bind.call(console.groupEnd, console);
     this.count = Function.prototype.bind.call(console.count, console, label);
     this.clear = Function.prototype.bind.call(console.clear, console);
