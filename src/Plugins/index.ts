8de4346e8ffafa78496b3dc23440477d83f43493
Andreas Schrottenbaum <andreas.schrottenbaum@3q.video>
2024-10-15 02:44:47 +0200

    cleanup

--- src/Plugins/index.ts
+++ src/Plugins/index.ts
@@ -1,4 +1,5 @@
-import { BasePlugin } from "./BasePlugin";
+import type { BasePlugin } from "./BasePlugin";
+
 import HeadlineFetcher from "./headline-fetcher";
 import PrestigePreview from "./prestive-preview";
 
